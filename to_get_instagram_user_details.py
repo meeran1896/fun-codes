b5e9f18b0492d2aff843fa7dd401bdbedab383c4
meeran1896 <80145573+meeran1896@users.noreply.github.com>
2021-10-06 16:38:18 +0800

    Update and rename oo.py to to_get_instagram_user_details.py

--- to_get_instagram_user_details.py
+++ to_get_instagram_user_details.py
@@ -1,3 +1,4 @@
+#install instagramy
 from instagramy import InstagramUser
 # user input
 name = input("Enter user name:")
@@ -12,3 +13,9 @@ print('Total followings:',following)
 # total number of posts
 posts = user.number_of_posts
 print('Total posts:',posts)
+#bio description
+bio = user.biography
+print('Bio:\n',bio)
+#link in bio
+link_in_bio = user.website
+print("Link in Bio:",link_in_bio)
