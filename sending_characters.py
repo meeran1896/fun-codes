816915c2e88a42b5db275de5f85a3901d49aff9a
Mohamed Meeran <80145573+meeran1896@users.noreply.github.com>
2022-01-13 01:36:14 +0800

    Update sending_characters.py

--- sending_characters.py
+++ sending_characters.py
@@ -29,3 +29,13 @@ for i in range(150):
     forward(5)
    
  
+#Create a virus
+from turtle import *
+bgcolor('black')
+color("cyan")
+speed(11)
+b=200
+while b > 0:
+  left(b)
+  forward(b*3)
+  b=b-1
