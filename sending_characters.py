762a450bf5700bb5a041fdd0f83168b11a7702fc
Mohamed Meeran <80145573+meeran1896@users.noreply.github.com>
2022-01-13 01:24:54 +0800

    Update and rename send_a_heart.py to sending_characters.py

---
+++ sending_characters.py
@@ -0,0 +1,31 @@
+#Sending a heart
+from turtle import *
+color('red')
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+
+#Producing infinity
+from turtle import *
+bgcolor('black')
+color("cyan")
+speed(11)
+right(45)
+for i in range(150):
+  circle(30)
+  if 7 < i < 62:
+    left(5)
+  if 80 < i < 33:
+    right(5)
+  if i < 80
+    forward(10)
+  else:
+    forward(5)
+   
+ 
