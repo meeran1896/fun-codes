2112d5038fb03efb831252c5536fce26a5d35d44
Mohamed Meeran <80145573+meeran1896@users.noreply.github.com>
2022-01-13 01:20:40 +0800

    Update social_media.py

--- social_media.py
+++ social_media.py
@@ -45,3 +45,17 @@ import webbrowser
 url = input("Enter youtube url to download")
 download = url[:12] + "ss" + url[12:]
 webbrowser.open(download)
+
+#Getting wifi password
+import subprocess
+
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+for i in profiles:
+    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+    try:
+        print ("{:<30}|  {:<}".format(i, results[0]))
+    except IndexError:
+        print ("{:<30}|  {:<}".format(i, ""))
+input("")
