7a7311203d246b12b3b479d0943931cd3f00af0a
Mohamed Meeran <80145573+meeran1896@users.noreply.github.com>
2022-01-13 02:40:47 +0800

    Create ceaser_cipher.py

---
+++ ceaser_cipher.py
@@ -0,0 +1,72 @@
+import re
+from collections import Counter
+
+def count_words(path):
+    with open(path, encoding='utf-8') as file:
+        all_words = re.findall(r"[0-9a-zA-Z-']+", file.read())
+        all_words = [word.upper() for word in all_words]
+        print('\nTotal Words:', len(all_words))
+        
+        word_counts = Counter()
+        for word in all_words:
+            word_counts[word] += 1
+        
+        print('\nTop 20 Words:')
+        for word in word_counts.most_common(20):
+            print(word[0], '\t', word[1])
+            
+if __name__ == '__main__':
+    count_words('C:\\Users\\Tanmay\\Desktop\\filename.txt')
+
+# Caesar Cipher
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+MAX_KEY_SIZE = len(SYMBOLS)
+
+def getMode():
+    while True:
+        print('Do you wish to encrypt or decrypt a message?')
+        mode = input().lower()
+        if mode in ['encrypt', 'e', 'decrypt', 'd']:
+            return mode
+        else:
+            print('Enter either "encrypt" or "e" or "decrypt" or "d".')
+
+def getMessage():
+    print('Enter your message:')
+    return input()
+
+def getKey():
+    key = 0
+    while True:
+        print('Enter the key number (1-%s)' % (MAX_KEY_SIZE))
+        key = int(input())
+        if (key >= 1 and key <= MAX_KEY_SIZE):
+            return key
+
+def getTranslatedMessage(mode, message, key):
+    if mode[0] == 'd':
+        key = -key
+    translated = ''
+
+    for symbol in message:
+        symbolIndex = SYMBOLS.find(symbol)
+        if symbolIndex == -1: # Symbol not found in SYMBOLS.
+            # Just add this symbol without any change.
+            translated += symbol
+        else:
+            # Encrypt or decrypt
+            symbolIndex += key
+
+            if symbolIndex >= len(SYMBOLS):
+                symbolIndex -= len(SYMBOLS)
+            elif symbolIndex < 0:
+                symbolIndex += len(SYMBOLS)
+
+            translated += SYMBOLS[symbolIndex]
+    return translated
+
+mode = getMode()
+message = getMessage()
+key = getKey()
+print('Your translated text is:')
+print(getTranslatedMessage(mode, message, key))
